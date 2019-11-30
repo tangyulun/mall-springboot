cff7ccd953a9813b663a880781fa7d4ed5475862
tangyulun <17673256238@163.com>
2019-11-30 16:01:56 +0800

    111

--- src/main/java/com/springboot/mall/service/UmsAdminService.java
+++ src/main/java/com/springboot/mall/service/UmsAdminService.java
@@ -1,14 +1,35 @@
 package com.springboot.mall.service;
 
+import com.springboot.mall.dto.UmsAdminParam;
+import com.springboot.mall.model.UmsAdmin;
+import com.springboot.mall.model.UmsPermission;
+
+import java.util.List;
+
 /**
  * UmsAdminService
  */
 public interface UmsAdminService {
   /**
-   * 登录
-   * @param username
-   * @param password
-   * @return
+   * 根据用户名获取后台管理员
    */
-  Integer login(String username,String password);
+  UmsAdmin getAdminByUsername(String username);
+
+  /**
+   * 注册功能
+   */
+  UmsAdmin register(UmsAdminParam umsAdminParam);
+
+  /**
+   * 登录功能
+   * @param username 用户名
+   * @param password 密码
+   * @return 生成的JWT的token
+   */
+  String login(String username, String password);
+
+  /**
+   * 获取用户所有权限（包括角色权限和+-权限）
+   */
+  List<UmsPermission> getPermissionList(Long adminId);
 }
--- src/main/java/com/springboot/mall/service/impl/UmsAdminServiceImpl.java
+++ src/main/java/com/springboot/mall/service/impl/UmsAdminServiceImpl.java
@@ -1,10 +1,21 @@
 package com.springboot.mall.service.impl;
 
 import com.springboot.mall.dao.UmsAdminMapper;
+import com.springboot.mall.dto.UmsAdminParam;
 import com.springboot.mall.model.UmsAdmin;
+import com.springboot.mall.model.UmsPermission;
 import com.springboot.mall.service.UmsAdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * UmsAdminService的实现类
@@ -12,15 +23,52 @@ import org.springframework.stereotype.Service;
 @Service
 public class UmsAdminServiceImpl implements UmsAdminService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(UmsAdminServiceImpl.class);
   @Autowired
-  private UmsAdminMapper umsAdminMapper;
+  private UserDetailsService userDetailsService;
+//  @Autowired
+//  private JwtTokenUtil jwtTokenUtil;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+  @Value("${jwt.tokenHead}")
+  private String tokenHead;
+  @Autowired
+  private UmsAdminMapper adminMapper;
+//  @Autowired
+//  private UmsAdminRoleRelationMapper adminRoleRelationMapper;
+//  @Autowired
+//  private UmsAdminRoleRelationMapper umsAdminRoleRelationMapper;
 
-  public Integer login(String username,String password){
+  @Override
+  public UmsAdmin getAdminByUsername(String username) {
+//    UmsAdminExample example = new UmsAdminExample();
+//    example.createCriteria().andUsernameEqualTo(username);
+//    List<UmsAdmin> adminList = adminMapper.selectByExample(example);
+//    if (adminList != null && adminList.size() > 0) {
+//      return adminList.get(0);
+//    }
+    return null;
+  }
+
+  @Override
+  public UmsAdmin register(UmsAdminParam umsAdminParam){
     UmsAdmin umsAdmin = new UmsAdmin();
-    umsAdmin.setUsername(username);
-    umsAdmin.setPassword(password);
-    Integer umsLogin = umsAdminMapper.toLogin(username,password);
-    return umsLogin;
+    BeanUtils.copyProperties(umsAdminParam,umsAdmin);
+    //umsAdmin.setId(new Long(6));
+    umsAdmin.setCreateTime(new Date());
+    umsAdmin.setStatus(1);
+    String md5Password = passwordEncoder.encode(umsAdmin.getPassword());
+    umsAdmin.setPassword(md5Password);
+    adminMapper.insert(umsAdmin);
+    return umsAdmin;
+  }
+  @Override
+  public String login(String username ,String password){
+    return null;
+  }
+  @Override
+  public List<UmsPermission> getPermissionList(Long adminId) {
+    return null;
   }
 
 }

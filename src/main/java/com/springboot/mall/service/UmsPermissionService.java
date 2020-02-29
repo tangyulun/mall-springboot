f12289fa084824ad0a35df2bc37193f8922b1d9b
tangyulun <17673256238@163.com>
2020-02-29 20:50:42 +0800

    用户权限管理

--- src/main/java/com/springboot/mall/service/UmsPermissionService.java
+++ src/main/java/com/springboot/mall/service/UmsPermissionService.java
@@ -1,6 +1,7 @@
 package com.springboot.mall.service;
 
 
+import com.springboot.mall.dto.UmsPermissionNode;
 import com.springboot.mall.model.UmsPermission;
 
 import java.util.List;
@@ -9,25 +10,30 @@ import java.util.List;
  * 后台用户权限管理Service
  */
 public interface UmsPermissionService {
-//    /**
-//     * 添加权限
-//     */
-//    int create(UmsPermission permission);
-//
-//    /**
-//     * 修改权限
-//     */
-//    int update(Long id, UmsPermission permission);
-//
-//    /**
-//     * 批量删除权限
-//     */
-//    int delete(List<Long> ids);
-//
-//    /**
-//     * 以层级结构返回所有权限
-//     */
-//    List<UmsPermissionNode> treeList();
+    /**
+     * 添加权限
+     * @param permission
+     * @return
+     */
+    int create(UmsPermission permission);
+    /**
+     * 修改权限
+     * @param id
+     * @param permission
+     * @return
+     */
+    int update(Long id, UmsPermission permission);
+    /**
+     * 批量删除权限
+     * @param ids
+     * @return
+     */
+    int delete(List<Long> ids);
+    /**
+     * 以层级结构返回所有权限
+     * @return
+     */
+    List<UmsPermissionNode> treeList();
     /**
      * 获取所有权限
      * @return
--- src/main/java/com/springboot/mall/service/impl/UmsPermissionServiceImpl.java
+++ src/main/java/com/springboot/mall/service/impl/UmsPermissionServiceImpl.java
@@ -1,13 +1,16 @@
 package com.springboot.mall.service.impl;
 
 import com.springboot.mall.dao.UmsPermissionMapper;
+import com.springboot.mall.dto.UmsPermissionNode;
 import com.springboot.mall.model.UmsPermission;
 import com.springboot.mall.model.UmsPermissionExample;
 import com.springboot.mall.service.UmsPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 后台用户权限管理Service实现类
@@ -16,38 +19,47 @@ import java.util.List;
 public class UmsPermissionServiceImpl implements UmsPermissionService {
     @Autowired
     private UmsPermissionMapper umsPermissionMapper;
-//
-//    @Override
-//    public int create(UmsPermission permission) {
-//        return 1;
-//    }
-//
-//    @Override
-//    public int update(Long id, UmsPermission permission) {
-//        return 1;
-//    }
-//
-//    @Override
-//    public int delete(List<Long> ids) {
-//        return 1;
-//    }
-//
-//    @Override
-//    public List<UmsPermissionNode> treeList() {
-//
-//        return null;
-//    }
+
+    @Override
+    public int create(UmsPermission permission) {
+        permission.setStatus(1);
+        permission.setCreateTime(new Date());
+        permission.setSort(0);
+        return umsPermissionMapper.insert(permission);
+    }
+
+    @Override
+    public int update(Long id, UmsPermission permission) {
+        permission.setId(id);
+        return umsPermissionMapper.updateByPrimaryKey(permission);
+    }
+
+    @Override
+    public int delete(List<Long> ids) {
+        UmsPermissionExample example = new UmsPermissionExample();
+        example.createCriteria().andIdIn(ids);
+        return umsPermissionMapper.deleteByExample(example);
+    }
+
+    @Override
+    public List<UmsPermissionNode> treeList() {
+        List<UmsPermission> permissionList = umsPermissionMapper.selectByExample(new UmsPermissionExample());
+        List<UmsPermissionNode> result = permissionList.stream()
+            .filter(permission -> permission.getPid().equals(0L))
+            .map(permission -> covert(permission,permissionList)).collect(Collectors.toList());
+        return result;
+    }
 
     @Override
     public List<UmsPermission> list() {
         return umsPermissionMapper.selectByExample(new UmsPermissionExample());
     }
 
-//    /**
-//     * 将权限转换为带有子级的权限对象
-//     * 当找不到子级权限的时候map操作不会再递归调用covert
-//     */
-//    private UmsPermissionNode covert(UmsPermission permission, List<UmsPermission> permissionList){
-//        return null;
-//    }
+    /**
+     * 将权限转换为带有子级的权限对象
+     * 当找不到子级权限的时候map操作不会再递归调用covert
+     */
+    private UmsPermissionNode covert(UmsPermission permission, List<UmsPermission> permissionList){
+        return null;
+    }
 }
